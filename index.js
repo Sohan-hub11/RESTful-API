5935423a6aacc05078fd1077e86572805e3d66d3
Sohan Samanta <sohansamanta18@gmail.com>
2025-12-11 03:44:51 +0530

    v4

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-let port = 8080;
+const port = 8080;
+const { v4 : uuidv4 } = require ("uuid");
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({extended: true}));
 
@@ -9,20 +11,21 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"/views"));
 
 app.use(express.static(path.join(__dirname,"/public")));
+app.use(methodOverride('_method'));
 
 let posts = [
   {
-    id : "1a",
+    id : uuidv4(),
     username : "sohansamanta18",
     content : "I love Coding"
   },
   {
-    id : "2b",
+    id : uuidv4(),
     username : "sintu11",
     content : "Time is most expensive thing"
   },
   {
-    id : "3c",
+    id : uuidv4(),
     username : "sohansamanta11",
     content : "I got my Job"
   }
@@ -42,14 +45,38 @@ app.get("/posts/new", (req,res) => {
 
 app.post("/posts", (req,res) => {
   let {username, content} = req.body;
-  posts.push({username, content});
+  let id = uuidv4();
+  posts.push({id, username, content});
   res.redirect("/posts");
 });
+
+app.get("/posts/:id/edit", (req, res) => {
+  let {id} = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", {post});
+})
 
 app.get("/posts/:id", (req,res) => {
   let {id} = req.params;
   let post = posts.find((p) => id === p.id);
   res.render("show.ejs", {post});
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let {id} = req.params;
+  let post = posts.find((p) => id === p.id);
+  if (!post) {
+    return res.status(404).send("Post not found");
+  }
+  post.content = req.body.content;
+  console.log(post);
+  res.redirect("/posts");
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let {id} = req.params;
+  posts = posts.filter((p) => id !== p.id);
+  res.redirect("/posts");
 });
 
 app.listen(port, () => {
